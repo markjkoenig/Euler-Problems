ed7f7478b97881f38ea487deb7a09e7f6916ace2
M. J. Koenig <koenigm410@gmail.com>
2021-09-10 00:45:59 -0400

    Fixed p1

--- prob1.py
+++ prob1.py
@@ -9,11 +9,11 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 
 def problem1(n):
 #Find sum of all multiples of 3 or 5 below 1000
-    x = []
+    x = 0
     for i in range(n):
-        if n%3 == 0 or n%5 == 0:
-            x.append(i)
-    return sum(x)
+        if i%3 == 0 or i%5 == 0:
+            x += i
+    return x
 
 #print('Sum of all multiples of 3 or 5 below 1000 is ' + str(problem1(1000)))
 
